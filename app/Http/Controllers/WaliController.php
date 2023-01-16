409889b8862d81cf85ade04869b00de41231f1b7
aspsptyd <devadiroot@gmail.com>
2023-01-16 22:03:32 +0700

    Update fungsi destroy WaliController.php

--- app/Http/Controllers/WaliController.php
+++ app/Http/Controllers/WaliController.php
@@ -136,7 +136,7 @@ class WaliController extends Controller
      */
     public function destroy($id)
     {
-        $model = Model::where('akses', 'wali')->findOrFail($id);
+        $model = Model::where('akses', 'wali')->firstOrFail();
 
         $model->delete();
         flash('Data berhasil dihapus');
