1a1ead7fb85ba3e31702571a09be3c00b1612313
aspsptyd <devadiroot@gmail.com>
2023-01-17 11:12:00 +0700

    Menambahkan View Data Siswa

---
+++ resources/views/operator/siswa_index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app_sneat')
+
+@section('content')
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <h5 class="card-header">Data Siswa</h5>
+            </div>
+        </div>
+    </div>
+@endsection
