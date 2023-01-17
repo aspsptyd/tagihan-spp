8f999f5bc6af69a890a0cad2eec6eb3b21b3190c
aspsptyd <devadiroot@gmail.com>
2023-01-17 11:10:15 +0700

    Tambah Controller untuk Data Siswa

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Siswa as Model; 
 
-class WaliController extends Controller
+class SiswaController extends Controller
 {
     private $viewIndex = 'siswa_index';
     private $viewCreate = 'siswa_form';
@@ -21,125 +21,6 @@ class WaliController extends Controller
      */
     public function index()
     {
-        return view('operator.' . $this->viewIndex, [
-            'models' => Model::where('akses', 'wali')->where('akses', '<>', '-')
-                ->latest()
-                ->paginate(50),
-                'routePrefix' => $this->routePrefix,
-                'title' => $this->accessClass
-        ]);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        $data = [
-            'model' => new Model(),
-            'method' => 'POST',
-            'route' => $this->routePrefix . '.store',
-            'button' => 'SIMPAN',
-            'title' => 'Tambah ' . $this->accessClass
-        ];
-        return view('operator.' . $this->viewCreate, $data);
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $requestData = $request->validate([
-            'name' => 'required',
-            'email' => 'required|unique:users',
-            'nohp' => 'required|unique:users',
-            'password' => 'required'
-        ]);
-        $requestData['password'] = bcrypt($requestData['password']);
-        $requestData['akses'] = 'wali';
-
-        Model::create($requestData);
-        flash('Data berhasil disimpan');
-        return redirect()->route($this->routePrefix . '.index');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        $data = [
-            'model' => \App\Models\User::findOrFail($id),
-            'method' => 'PUT',
-            'route' => [$this->routePrefix . '.update', $id],
-            'button' => 'UPDATE',
-            'title' => 'Ubah ' . $this->accessClass
-        ];
-        return view('operator.' . $this->viewEdit, $data);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        $requestData = $request->validate([
-            'name' => 'required',
-            'email' => 'required|unique:users,email,' . $id,
-            'nohp' => 'required|unique:users,nohp,' . $id,
-            'password' => 'nullable'
-        ]);
-        $model = Model::findOrFail($id);
-        if ($requestData['password'] == "") {
-            unset($requestData['password']);
-        } else {
-            $requestData['password'] = bcrypt($requestData['password']);
-        }
-        $requestData['akses'] = 'wali';
-
-        $model->fill($requestData);
-        $model->save();
-
-        flash('Data berhasil diubah');
-        return redirect()->route($this->routePrefix . '.index');
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        $model = Model::where('akses', 'wali')->firstOrFail();
-
-        $model->delete();
-        flash('Data berhasil dihapus');
-        return back();
+        return view('operator.' . $this->viewIndex);
     }
 }
